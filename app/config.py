f699457dcd8cddfbb7f02a35e69196c68cc3e613
Mohammed <maaz19499@gmail.com>
2024-05-29 11:00:19 +0530

    Initial Commit

--- app/config.py
+++ app/config.py
@@ -6,15 +6,7 @@ import logging
 
 FIREBASE_CRED_PATH = 'app/utils/firebase_cred/airbnbbot-usmaanclient-firebase-adminsdk-obzqc-78e56060b5.json'
 
-ACCESS_TOKEN="EABpkf677ZBeoBO036aTJbq0FA65P8Q6bN6QgwEEAOf0aPdaUKHeiA2qVA2fnIQ8icyONvpQfs2JVUY92lptdvip74NV3BvFO8fxu9m8VrTZBNhJGa5PTqyo4lGuLkKGYjeaTNOfoHA7xcyhhj1cKkvNixzMAqCmy85X3mqZCsbZCPcgCTCFgnCYwYlfJBVBYBnXGzWjVP2RMfWEuDUoZD"
 
-APP_ID="7428848953850346"
-APP_SECRET="538fd8b469754edf682fff536303a9b1"
-RECIPIENT_WAID="+919945657981"
-VERSION="v19.0"
-PHONE_NUMBER_ID="290500427483399"
-VERIFY_TOKEN="project_novus"
-Manager_WAID = "917338006388"
 
 
 def load_configurations(app):
